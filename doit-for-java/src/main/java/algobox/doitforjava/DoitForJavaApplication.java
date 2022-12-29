69832b05b2e534b93e5bbbd26c9045d2d38ddcf7
ahn-sj <jaesa5221@gmail.com>
2022-12-29 14:07:32 +0900

    Begin: Init Algorithm Project

---
+++ doit-for-java/src/main/java/algobox/doitforjava/DoitForJavaApplication.java
@@ -0,0 +1,13 @@
+package algobox.doitforjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitForJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitForJavaApplication.class, args);
+	}
+
+}
